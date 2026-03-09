b89d1e8200432d03657b3c9afc42183cea72b607
castral <castral01@gmail.com>
2026-03-09 15:18:31 -0500

    Updating to vite to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite';
 
 export default defineConfig({
-    test: {
-      name: "ts-enum",
-      root: "./test",
-      globals: true,
-      environment: "node",
-    },
-  }
-);
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    target: 'esnext',
+    lib: {
+      entry: './lib/index.ts',
+      fileName: 'index',
+      formats: ['es'],
+    }
+  },
+  test: {
+    name: 'ts-enum',
+    root: './test',
+    globals: true,
+    environment: 'node',
+  },
+} satisfies UserConfig);
